c7199fd75020756c26b32d5f21ba1628ab1ec995
JBisquick <felipebirch@gmail.com>
2023-08-15 14:21:36 -0700

    Add function playRound
    Function declares winner of the round.

--- script.js
+++ script.js
@@ -6,6 +6,26 @@ function getComputerChoice() {
 		case 1: 
 			return 'paper';
 		case 2:
-			return 'scissors';
+			return 'scissor';
+	}
+}
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection.toLowerCase()
+	switch (true) {
+		case playerSelection === 'rock' && computerSelection === 'scissor':
+		case playerSelection === 'paper' && computerSelection === 'rock':
+		case playerSelection === 'scissor' && computerSelection === 'paper':
+			playerSelection = playerSelection.toUpperCase().slice(0,1) + playerSelection.slice(1);
+			return `You Win! ${playerSelection} beats ${computerSelection}.`;
+		case playerSelection === 'rock' && computerSelection === 'rock': 
+		case playerSelection === 'paper' && computerSelection === 'paper':
+		case playerSelection === 'scissor' && computerSelection === 'scissor':
+			return `Draw! Both chose ${playerSelection}.`
+		case playerSelection === 'rock' && computerSelection === 'paper':
+		case playerSelection === 'paper' && computerSelection === 'scissor':
+		case playerSelection === 'scissor' && computerSelection === 'rock':
+			computerSelection = computerSelection.slice(0,1).toUpperCase() + computerSelection.slice(1);
+			return `You Lose! ${computerSelection} beats ${playerSelection}.`;
 	}
 }
